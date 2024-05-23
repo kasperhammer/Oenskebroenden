fff8a460d37087bf29d71746498f68a3eff46514
Kasper <Kasper@DESKTOP-G01L98C>
2024-05-23 16:32:08 +0200

    sd

---
+++ OenskeBroenden/Components/Pages/LogoutPage.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using OenskeBroenden.Utils;
+
+namespace OenskeBroenden.Components.Pages
+{
+    [Authorize]
+    public partial class LogoutPage
+    {
+        [Inject]
+        Auth Auth { get; set; }
+
+        [Inject]
+        NavigationManager NavMan { get; set; }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await Auth.LogoutAsync();
+                NavMan.NavigateTo("/login");
+            }
+        }
+    }
+}
